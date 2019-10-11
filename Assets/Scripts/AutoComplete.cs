bd902cd17cc05ee7392c2b3001ae91de7accac3f
julien <j.crepin@rubika-edu.com>
2019-10-11 16:25:27 +0200

    connection BD zone a trou --> travaille sur l'autocompletion

---
+++ Assets/Scripts/AutoComplete.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine.UI;
+
+
+public class AutoComplete : MonoBehaviour
+{
+    public int LenghtAutoComplete = 3;
+
+    private InputField mot;
+    private String Connection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connection = "URI=file:" + Application.dataPath + "/BD/mot_emotions.db";
+        mot = GetComponent<InputField>();
+    }
+
+    public void AutoComp ()
+    {
+        if (mot.text.Length >= LenghtAutoComplete)
+        {
+            using (IDbConnection dbConnection = new SqliteConnection(Connection))
+            {
+                dbConnection.Open();
+
+                using (IDbCommand dbCmd = dbConnection.CreateCommand())
+                {
+                    String sqlQuery = "SELECT * FROM Mots";
+
+                    dbCmd.CommandText = sqlQuery;
+
+                    using (IDataReader reader = dbCmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Debug.Log(mot.text + "  " + reader["Name"]);
+                        }
+
+                        dbConnection.Close();
+                        reader.Close();
+                    }
+
+                }
+            }
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/ScoreBD.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine.UI;
+
+public class ScoreBD : MonoBehaviour
+{
+    public InputField Mot1;
+    public InputField Mot2;
+    public InputField Mot3;
+
+    private String Connection;
+    private List<InputField> ListeMots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connection = "URI=file:" + Application.dataPath + "/BD/mot_emotions.db";
+        ListeMots = new List<InputField>();
+        ListeMots.Add(Mot1);
+        ListeMots.Add(Mot2);
+        ListeMots.Add(Mot3);
+    }
+
+    public void GetElemBd()
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(Connection))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                foreach (InputField elem in ListeMots)
+                {
+                    String sqlQuery = "SELECT * FROM Mots WHERE Name = '" + elem.text + "'";
+
+                    dbCmd.CommandText = sqlQuery;
+
+                    using (IDataReader reader = dbCmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Debug.Log(elem.text + " = " + reader["ScorePers1"]);
+                        }
+
+                        reader.Close();
+                    }
+                }
+
+                dbConnection.Close();
+            }
+        }
+    }
+}
