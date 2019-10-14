f5475cd7f39ddcccc52a75bde02b004a0a981cb4
Nils Vandamme <nils.vandamme98@gmail.com>
2019-10-14 17:13:17 +0200

    Drag&drop completed

--- Assets/Scripts/DragDropControls.cs
+++ Assets/Scripts/DragDropControls.cs
@@ -2,22 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// This script make the object attached to drag&drop-able
+// This script make the object attached to drag & drop-able
 
 public class DragDropControls : MonoBehaviour
 {
-    public float HoveringHeight = -5.25f;
-    public float SpeedDivider = 2.5f;
+    [Header("System values")]
 
-    private Vector3 mouseOffset;
+    public float HoveringHeight = -5.25f; // Height at which the object will hover while being dragged
+    public float SpeedDivider = 2.5f; // Divide the drag speed
+    public float SnapSpeed; // Speed of the snapping movement
+
+    [Header("Debug")]
+
+    public bool IsSnapping; // Is the object being snapped ?
+    public Vector3 SnapPosition; // Position at which the object snaps
+    public bool snapMovementActive; // Is the object moving to it's snap position ?
+
+    private Vector3 mouseOffset; 
     private float mouseZCoord;
-    private Rigidbody rig;
+    private Rigidbody rig; // Object rigidbidy
 
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        // Snap the object to it's target
+        if (snapMovementActive == true && Vector3.Distance(transform.position, SnapPosition) >= 0.03)
+        {
+            rig.position = Vector3.Lerp(rig.position, SnapPosition, SnapSpeed*Time.deltaTime);
+        }
+        // Unsnap the object from it's target
+        else if(snapMovementActive == true && Vector3.Distance(transform.position, SnapPosition) <= 0.03)
+        {
+            snapMovementActive = false;
+            rig.useGravity = true;
+        }
+    }
+
+    // When the mouse is being pressed
     private void OnMouseDown()
     {
         mouseZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
@@ -28,11 +53,29 @@ public class DragDropControls : MonoBehaviour
         rig.useGravity = false;
     }
 
+    // When the mouse is being released
     private void OnMouseUp()
     {
-        rig.useGravity = true;
+        if(IsSnapping == true)
+        {
+            snapMovementActive = true;
+        }
+        else
+        {
+            rig.useGravity = true;
+            snapMovementActive = false;
+        }
     }
 
+
+    // When the mouse is kept being pressed
+    private void OnMouseDrag()
+    {
+        rig.position = new Vector3(GetMouseWorldPos().x + mouseOffset.x, Mathf.Lerp(transform.position.y, HoveringHeight, Time.deltaTime * 2), GetMouseWorldPos().z + mouseOffset.z );
+    }
+
+
+    // System
     private Vector3 GetMouseWorldPos()
     {
         Vector3 mousePoint = Input.mousePosition;
@@ -40,10 +83,5 @@ public class DragDropControls : MonoBehaviour
         mousePoint.z = mouseZCoord;
 
         return Camera.main.ScreenToWorldPoint(mousePoint);
-    }
-
-    private void OnMouseDrag()
-    {
-        rig.position = new Vector3(GetMouseWorldPos().x + mouseOffset.x, Mathf.Lerp(transform.position.y, HoveringHeight, Time.deltaTime * 2), GetMouseWorldPos().z + mouseOffset.z );
     }
 }
---
+++ Assets/Scripts/PaperSnap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script make the paragraphs snap on the paper when drag & dropped
+
+public class PaperSnap : MonoBehaviour
+{
+    public bool HasSnap; // Is an object currently snapped to this object ?
+    public GameObject SnappedObject;
+
+    // Snap the object
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(gameObject.name + " has collided with " + other.name);
+        if (other.tag == "Paragraph" && SnappedObject == null)
+        {
+            //Debug.Log(gameObject.name + " has snapped with " + other.name);
+            SnappedObject = other.gameObject;
+            other.GetComponent<DragDropControls>().IsSnapping = true;
+            other.GetComponent<DragDropControls>().SnapPosition = transform.position;
+            HasSnap = true;
+        }
+
+    }
+
+    // Unsnap the object
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log(gameObject.name + " has ended collision with " + other.name);
+        if (other.tag == "Paragraph" && other.name == SnappedObject.name)
+        {
+            //Debug.Log(gameObject.name + " has unsnapped with " + other.name);
+            SnappedObject = null;
+            other.GetComponent<DragDropControls>().IsSnapping = false;
+            other.GetComponent<DragDropControls>().snapMovementActive = false;
+            HasSnap = false;
+        }
+
+    }
+}
