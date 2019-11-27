19b2acd9d86c059b950eee007692b3fae1aab2fe
julien <j.crepin@rubika-edu.com>
2019-11-27 17:09:12 +0100

    Non doublon

--- Assets/Scripts/PullOfWords/SC_InfoParagrapheOrdi.cs
+++ Assets/Scripts/PullOfWords/SC_InfoParagrapheOrdi.cs
@@ -9,21 +9,27 @@ public class SC_InfoParagrapheOrdi : MonoBehaviour
 
     public void OnClickParagrapheOrdi()
     {
-        Debug.Log(gameObject.name + " paragraph has been clicked");
         button.text = info.champLexical.fichierWords.name;
         button.gameObject.SetActive(true);
     }
 
     public void OnClickButtonConfirm()
     {
-        Debug.Log(gameObject.name + " button has been clicked");
         bool[] tabBool = new bool[SC_GM_Master.gm.listChampsLexicaux.listChampsLexicals[0].words[0].score.Length];
         for (int i = 0; i < info.motAccepter.Length; i++)
             if (info.motAccepter[i])
             {
                 SC_WordInPull elem = new SC_WordInPull(info.champLexical.fichierWords.name, info.champLexical.words[i], tabBool);
-                if (!SC_GM_Master.gm.choosenWords.Contains(elem))
-                    SC_GM_Master.gm.choosenWords.Add(elem);
+
+                foreach (SC_WordInPull wordPull in SC_GM_Master.gm.choosenWords)
+                    if (wordPull.GetCL() == elem.GetCL())
+                    {
+                        SC_GM_Master.gm.choosenWords.Remove(wordPull);
+                        SC_GM_Master.gm.choosenWords.Add(elem);
+                        return;
+                    }
+
+                SC_GM_Master.gm.choosenWords.Add(elem);
             }
     }
 }
